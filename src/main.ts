33ba806a6cb444f0b2f01476a010df1868fd50d1
WhitedogWarren <werquin.lucas@wanadoo.fr>
2025-08-12 08:53:59 +0200

    fix: :bug: add CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: { origin: true },
+  });
 
   app.useStaticAssets(join(__dirname, '..', 'assets'), {
     prefix: '/assets/',
